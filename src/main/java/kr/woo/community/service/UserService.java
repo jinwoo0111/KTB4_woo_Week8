ff8fa02c1c67363bb6139fd14dc29cf0d9899cac
jinwoo0111 <whi750105@naver.com>
2026-07-07 22:10:37 +0900

    password 암호화 적용

--- src/main/java/kr/woo/community/service/UserService.java
+++ src/main/java/kr/woo/community/service/UserService.java
@@ -2,14 +2,13 @@ package kr.woo.community.service;
 
 import kr.woo.community.dto.*;
 import kr.woo.community.exception.LoginFailedException;
-import kr.woo.community.exception.PasswordMismatchException;
 import kr.woo.community.exception.UserNotFoundException;
-import kr.woo.community.repository.PostRepository;
 import kr.woo.community.repository.UserRepository;
 import kr.woo.community.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
-
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public User findById(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException());
@@ -32,16 +31,17 @@ public class UserService {
         return userRepository.getReferenceById(id);
     }
 
-    // 회원가입 처리
-    // 중복 email 체크 & 중복 nickname 체크
+
     @Transactional
     public UserSignupResponse signup(UserSignupRequest request) {
 
         validateDuplicateEmail(request.getEmail());
         validateDuplicateNickname(request.getNickname());
 
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
         User user = new User(request.getEmail(),
-                request.getPassword(),
+                encodedPassword,
                 request.getNickname(),
                 request.getProfileImage()
         );
@@ -51,21 +51,18 @@ public class UserService {
         );
     }
 
-    // 중복 email 체크
     private void validateDuplicateEmail(String email) {
         if (userRepository.existsByEmail(email)) {
             throw new IllegalArgumentException("이미 사용중인 이메일입니다.");
         }
     }
 
-    // 중복 nickname 체크
     private void validateDuplicateNickname(String nickname) {
         if (userRepository.existsByNickname(nickname)) {
             throw new IllegalArgumentException("이미 사용중인 닉네임입니다");
         }
     }
 
-    // 로그인 처리
     public UserLoginResponse login(UserLoginRequest request) {
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new LoginFailedException());
@@ -74,7 +71,7 @@ public class UserService {
             throw new LoginFailedException();
         }
 
-        if (!user.getPassword().equals(request.getPassword())) {
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new LoginFailedException();
         }
 
@@ -83,7 +80,6 @@ public class UserService {
         );
     }
 
-    // 회원정보 수정 처리
     @Transactional
     public UserUpdateResponse updateUser(Long userId, UserUpdateRequest request) {
         User user = findById(userId);
@@ -101,13 +97,14 @@ public class UserService {
         );
     }
 
-    // 비밀번호 수정 처리
     @Transactional
     public void updatePassword(Long userId, UserPasswordUpdateRequest request) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
-        user.changePassword(request.getNewPassword());
+        String encodedPassword = passwordEncoder.encode(request.getNewPassword());
+
+        user.changePassword(encodedPassword);
     }
 
     // 회원탈퇴 처리
