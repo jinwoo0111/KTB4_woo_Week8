24cc87d3152af56a543fdd8875271c8063a03be3
jinwoo0111 <whi750105@naver.com>
2026-07-09 15:29:49 +0900

    회원정보 수정 테스트 작성

--- src/test/java/kr/woo/community/UserTest.java
+++ src/test/java/kr/woo/community/UserTest.java
@@ -2,6 +2,7 @@ package kr.woo.community;
 
 import kr.woo.community.dto.*;
 import kr.woo.community.entity.User;
+import kr.woo.community.exception.UserNotFoundException;
 import kr.woo.community.repository.UserRepository;
 import kr.woo.community.service.UserService;
 import org.junit.jupiter.api.DisplayName;
@@ -102,5 +103,67 @@ public class UserTest {
         assertEquals("newProfileImage", user.getProfileImage());
     }
 
+    @Test
+    @DisplayName("프로필 이미지만 수정하면 닉네임은 유지되고 프로필 이미지만 변경된다")
+    void updateProfileImageSuccess() {
+        Long userId = 1L;
+
+        User user = new User(
+                "test@test.com",
+                "Test1234!",
+                "old닉네임",
+                "oldProfileImage"
+        );
+
+        UserUpdateRequest request = new UserUpdateRequest(
+                null,
+                "newProfileImage"
+        );
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        userService.updateUser(userId, request);
+        assertEquals("old닉네임", user.getNickname());
+        assertEquals("newProfileImage", user.getProfileImage());
+    }
+
+    @Test
+    @DisplayName("닉네임만 수정하면, profileImage는 유지되고 닉네임만 수정된다")
+    void updateNicknameSuccess() {
+        Long userId = 1L;
+
+        User user = new User(
+                "test@test.com",
+                "Test1234!",
+                "old닉네임",
+                "oldProfileImage"
+        );
+
+        UserUpdateRequest request = new UserUpdateRequest(
+                "new닉네임",
+                null
+        );
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        userService.updateUser(userId, request);
+        assertEquals("new닉네임", user.getNickname());
+        assertEquals("oldProfileImage", user.getProfileImage());
+    }
+
+    @Test
+    @DisplayName("회원정보 수정 시, 유저가 존재하지 않으면 예외가 발생한다")
+    void updateUserFail() {
+         Long userId = 1L;
+         UserUpdateRequest request = new UserUpdateRequest(
+                 null,
+                 null
+         );
+         when(userRepository.findById(userId)).thenReturn(Optional.empty());
+         assertThrows(UserNotFoundException.class, ()-> {
+             userService.updateUser(userId, request);
+         });
+
+    }
 }
+
 
