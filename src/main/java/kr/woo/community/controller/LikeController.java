49bb8ec7661f84f3fae85967341764b739ce7d8d
jinwoo0111 <whi750105@naver.com>
2026-07-09 22:29:19 +0900

    좋아요 관련 인가 추가

--- src/main/java/kr/woo/community/controller/LikeController.java
+++ src/main/java/kr/woo/community/controller/LikeController.java
@@ -1,14 +1,14 @@
 package kr.woo.community.controller;
 
 
-import jakarta.validation.Valid;
 import kr.woo.community.common.ApiResponse;
-import kr.woo.community.dto.PostLikeRequest;
 import kr.woo.community.service.PostService;
+import kr.woo.community.security.user.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +19,9 @@ public class LikeController {
     @PostMapping("/posts/{postId}/likes")
     public ResponseEntity<ApiResponse<Void>> createLike (
             @PathVariable Long postId,
-            @Valid @RequestBody PostLikeRequest request
+            @AuthenticationPrincipal CustomUserDetails loginUser
     ) {
-        postService.createLike(postId, request.getUserId());
+        postService.createLike(postId, loginUser.getId());
         ApiResponse<Void> response = new ApiResponse<>(
                 "like_create_success",
                 null
@@ -34,9 +34,9 @@ public class LikeController {
     @DeleteMapping("/posts/{postId}/likes")
     public ResponseEntity<ApiResponse<Void>> deleteLike (
             @PathVariable Long postId,
-            @Valid @RequestBody PostLikeRequest request
+            @AuthenticationPrincipal CustomUserDetails loginUser
     ) {
-        postService.deleteLike(postId, request.getUserId());
+        postService.deleteLike(postId, loginUser.getId());
         ApiResponse<Void> response = new ApiResponse<>(
                 "post_like_delete_success",
                 null
