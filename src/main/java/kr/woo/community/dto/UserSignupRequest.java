0131f1f08a0a2fab545653330ec6bdc22817dd27
jinwoo0111 <whi750105@naver.com>
2026-07-09 14:35:11 +0900

    회원가입 테스트 코드

--- src/main/java/kr/woo/community/dto/UserSignupRequest.java
+++ src/main/java/kr/woo/community/dto/UserSignupRequest.java
@@ -3,12 +3,14 @@ package kr.woo.community.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 // 회원가입 요청 DTO
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserSignupRequest {
     @NotBlank
     @Email
---
+++ src/test/java/kr/woo/community/UserTest.java
@@ -0,0 +1,77 @@
+package kr.woo.community;
+
+import kr.woo.community.dto.*;
+import kr.woo.community.entity.User;
+import kr.woo.community.repository.UserRepository;
+import kr.woo.community.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    @DisplayName("회원가입 시 이메일이 중복되면 예외가 발생해야한다")
+    void validateDulplicateEmailSuccess() {
+
+        UserSignupRequest request = new UserSignupRequest("test@test.com", "Test1234!", "Test계정", "profile_image");
+        when(userRepository.existsByEmail(anyString())).thenReturn(true);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            userService.signup(request);
+        });
+
+        verify(userRepository, times(1)).existsByEmail(anyString());
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("회원가입 시 닉네임이 중복되면 예외가 발생해야한다")
+    void validateDulplicateNicknameSuccess() {
+
+        UserSignupRequest request = new UserSignupRequest("test@test.com", "Test1234!", "Test계정입니다", "profile_image");
+
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
+        when(userRepository.existsByNickname(anyString())).thenReturn(true);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            userService.signup(request);
+        });
+
+        verify(userRepository, times(1)).existsByNickname(anyString());
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("회원가입에 성공하여 유저 저장을 요청한다")
+    void signupSuccess() {
+        UserSignupRequest request = new UserSignupRequest("test@test.com", "Test1234!", "Test계정입니다", "profile_image");
+
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
+        when(userRepository.existsByNickname(anyString())).thenReturn(false);
+        when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
+
+        userService.signup(request);
+
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+}
+
