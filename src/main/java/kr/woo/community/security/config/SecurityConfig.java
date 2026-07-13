af99c6ebafcb2365810981b1ec33b1991b34f946
jinwoo0111 <whi750105@naver.com>
2026-07-13 11:38:46 +0900

    허용 주소 추가

--- src/main/java/kr/woo/community/security/config/SecurityConfig.java
+++ src/main/java/kr/woo/community/security/config/SecurityConfig.java
@@ -44,7 +44,9 @@ public class SecurityConfig {
         return request -> {
             CorsConfiguration configuration = new CorsConfiguration();
             configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500",
-                    "http://localhost:5500"));
+                    "http://localhost:5500",
+                    "http://127.0.0.1:5501",
+                    "http://localhost:5501"));
             configuration.setAllowedMethods(Collections.singletonList("*"));
             configuration.setAllowCredentials(true);
             configuration.setAllowedHeaders(Collections.singletonList("*"));
